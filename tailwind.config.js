24ab17115f3bae5e758f52b323aad7ae777bd5b5
Chandrakant-Nagwanshi <chandrakantnagwanshi97@gmail.com>
2024-02-17 19:36:18 +0530

    Add CartItem

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Robot Mono,monospace ",
+    },
+    extend: {
+      height: {
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
